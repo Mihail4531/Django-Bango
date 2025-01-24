de5ede000545c4dcc0ea994a26ce2e4c52a12455
Абросимов Михаил Сергеевич <abrosimov4brosimov@yandex.ru>
2025-01-24 13:31:31 +0300

    Второе приложение

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace="blog"))
+    path('', include('blog.urls', namespace="blog")),
+    path('', include('porfolio.urls', namespace="porfolio"))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
